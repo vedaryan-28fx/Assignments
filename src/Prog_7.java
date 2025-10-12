ded9efd0eea46c943e76e83423fddefa7c71c90f
vedaryan-28fx <github28fx.com>
2025-10-12 12:36:58 +0530

    init commit

--- src/Prog_7.java
+++ src/Prog_7.java
@@ -1,5 +1,17 @@
 public class Prog_7 {
     public static void main(String[] args) {
+        System.out.println("a\tb\tpow(a, b)");
 
+        int a1 = 1, b1 = 2;
+        int a2 = 2, b2 = 3;
+        int a3 = 3, b3 = 4;
+        int a4 = 4, b4 = 5;
+        int a5 = 5, b5 = 6;
+
+        System.out.println(a1 + "\t" + b1 + "\t" + (int)Math.pow(a1, b1));
+        System.out.println(a2 + "\t" + b2 + "\t" + (int)Math.pow(a2, b2));
+        System.out.println(a3 + "\t" + b3 + "\t" + (int)Math.pow(a3, b3));
+        System.out.println(a4 + "\t" + b4 + "\t" + (int)Math.pow(a4, b4));
+        System.out.println(a5 + "\t" + b5 + "\t" + (int)Math.pow(a5, b5));
     }
 }
